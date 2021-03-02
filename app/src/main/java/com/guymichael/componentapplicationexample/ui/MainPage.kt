28935c96de22a5a8243e159b6aa6d37c7c953008
Guy Michael <guy@engieapp.com>
2021-03-02 19:43:03 +0200

    Updated alert dialog creation to the new style

--- app/src/main/java/com/guymichael/componentapplicationexample/ui/MainPage.kt
+++ app/src/main/java/com/guymichael/componentapplicationexample/ui/MainPage.kt
@@ -6,10 +6,11 @@ import com.guymichael.componentapplicationexample.store.MainStore
 import com.guymichael.componentapplicationexample.store.reducers.GeneralReducerKey
 import com.guymichael.kotlinreact.model.EmptyOwnProps
 import com.guymichael.kotlinreact.model.EmptyOwnState
+import com.guymichael.reactdroid.core.getString
 import com.guymichael.reactdroid.core.model.AHOC
 import com.guymichael.reactdroid.core.model.ASimpleComponent
-import com.guymichael.reactdroid.extensions.components.dialog.dialog
-import com.guymichael.reactdroid.extensions.components.dialog.with
+import com.guymichael.reactdroid.extensions.components.dialog.AlertDialogProps
+import com.guymichael.reactdroid.extensions.components.dialog.withAlertDialog
 import com.guymichael.reactdroidflux.model.connect
 
 class MainPage(v: View) : ASimpleComponent<MainPageProps>(v) {
@@ -17,17 +18,18 @@ class MainPage(v: View) : ASimpleComponent<MainPageProps>(v) {
     //this dialog can even be a member of the MyApplication which will wait for the first Activity that opens
     // (whatever it is) and show a dialog on it. Good for dialogs which should be shown no matter what
     // (e.g. loggedIn or not, got from a deepLink to non-main page, etc.)
-    private val cDialog = with(lazy { dialog(
-            positiveBtn = R.string.dialog_btn_ok to {}
-            , negativeBtn = null
-            , title = R.string.home_dialog_title
-            , message = R.string.home_dialog_msg
-        )}
-        , onDismiss = { MainStore.dispatch(GeneralReducerKey.welcomeDialogShown, false) }
+    private val cDialog = withAlertDialog(
+        onDismiss = { MainStore.dispatch(GeneralReducerKey.welcomeDialogShown, false) }
     )
 
     override fun render() {
-        cDialog.onRender(props.welcomeDialogShown)
+        cDialog.onRender(AlertDialogProps(
+            props.welcomeDialogShown
+            , title = getString(R.string.home_dialog_title)
+            , message = getString(R.string.home_dialog_msg)
+            , okBtn = getString(R.string.dialog_btn_ok) to { _ -> null}
+            , cancelBtn = null
+        ))
     }
 
 
